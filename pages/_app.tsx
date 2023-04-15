62fad26f34d386289b6dc245589a52654be65d19
Sean Fong <seancfong@gmail.com>
2023-04-15 16:11:52 -0700

    Add search bar and font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Cardo, Manrope } from "next/font/google";
+
+const cardo = Cardo({ subsets: ["latin"], weight: ["400", "700"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --cardo-font: ${cardo.style.fontFamily};
+            --manrope-font: ${manrope.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
